f955e218aa6ca7bd7f4a16981c6fb288b16f6b21
cvdavenportperkins <cvdavenportperkins@gmail.com>
2025-04-04 07:55:01 -0400

    Friday 4.4.25

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,33 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameManager instance = null;
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI youWinText;
+    public TextMeshProUGUI gameOverText;
+    public float timer;
+    private bool isGameOver = false;
+    public float gameTime = 120;
+
+
+    private void Awake()
+    {
+        // Check if instance already exists
+        if (instance == null)
+        {
+            instance = this; // Set instance to this
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject); // Destroy duplicate
+        }
+
+        // Make GameManager persistent across scenes
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
